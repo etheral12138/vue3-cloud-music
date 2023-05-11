818176654004ec4c2db0fbbad116ff5f8a113b04
etheral12138 <1694815257@qq.com>
2023-05-11 18:43:16 +0800

    fix:优化布局

--- src/service/playlist.ts
+++ src/service/playlist.ts
@@ -100,11 +100,11 @@ export function getSimilarPlaylist(id:string) {
 }
 // 相似歌曲
 export function getSimilarSong(id:string) {
-  const res = service.get('/simi/song?id=' + id);
-  res.data.songs.forEach(song => {
-    song.mp3Url = `https://${song.mp3Url.slice(7)}`;
-  });
-  return res;
+  // const res = service.get('/simi/song?id=' + id);
+  // res.data.songs.forEach(song => {
+  //   song.mp3Url = `https://${song.mp3Url.slice(7)}`;
+  // });
+  return service.get('/simi/song?id=' + id);
 }
 //对歌单添加或删除歌曲
 export function updatePlaylistTracks(data:{
